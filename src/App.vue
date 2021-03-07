0fb8eaaa003f7bd44c9f8be13b46c4e705486521
delongchen <fg456@vip.qq.com>
2021-03-07 23:24:35 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <p @click="w += 100">hello</p>
+    <temp :width="w"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Temp from '@/components/TempChart'
+import {ref} from 'vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Temp
+  },
+  setup() {
+    const w = ref(500)
+
+    return { w }
   }
 }
 </script>
@@ -21,6 +29,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/TempChart/index.vue
+++ src/components/TempChart/index.vue
@@ -1,10 +1,31 @@
 <template>
-$END$
+  <p>{{ height }}</p>
+  <p>{{ chartHeight }}</p>
+  <p>{{ chartWidth }}</p>
 </template>
 
 <script>
+import { computed } from 'vue'
+
+const def = v => ({ default() {return v}})
+
 export default {
-name: "index"
+  name: "TempChart",
+  props: {
+    width: def(500)
+  },
+  setup(props) {
+    const margin = { left: 20, bottom: 20, right: 60, top: 10 },
+      height = computed(() => props.width * .5),
+      chartWidth = computed(() => props.width - margin.left - margin.right),
+      chartHeight = computed(() => height.value - margin.top - margin.bottom)
+
+    return {
+      height,
+      chartWidth,
+      chartHeight
+    }
+  }
 }
 </script>
 
